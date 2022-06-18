900b5575cb71b70c05002a9b181acc03f94b227f
Strelkova Olga <strelod@yandex.ru>
2022-06-18 19:52:22 +0300

    start refacoring

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const { PORT = 3000 } = process.env
+
+const { PORT = 3000 } = process.env;
 const app = express();
+
 app.listen(PORT, () => {
-    console.log(PORT)
+  console.log('Ссылка на сервер:');
+  console.log(BASE_PATH);
+});
+
+app.get('/animals', (req, res) => {
+  res.send(animals)
 })
