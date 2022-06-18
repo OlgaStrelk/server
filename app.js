ac1e046965f4321dc1f4409aec1a3562b00a0994
Strelkova Olga <strelod@yandex.ru>
2022-06-18 20:19:51 +0300

    add mainpage router

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const routes = require('./routes')
+
+
+app.use('/', routes)
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
   console.log(BASE_PATH);
---
+++ routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+// const { users } = require('./db.js');
+
+
+module.exports = router;
