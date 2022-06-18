5da0bc3399218bb0be6f661f59ba4b5b4b6c9843
Strelkova Olga <strelod@yandex.ru>
2022-06-18 19:55:03 +0300

    add files

---
+++ db.js
@@ -0,0 +1,12 @@
+const animals = {
+    dog: {
+      type1: 'chihuahua',
+      type2: 'bloodhound',
+      type3: 'german shepherd'
+    },
+    cat: {
+      type1: 'abyssinian',
+      type2: 'dwelf',
+      type3: 'highlander'
+    }
+  };
