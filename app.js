f499f5aad03a1d4c7aa3237cb4c96f7b4e40c90f
Strelkova Olga <strelod@yandex.ru>
2022-06-21 17:38:57 +0300

    add middleware

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
 const { PORT = 3000 } = process.env;
 const app = express();
-
 const routes = require('./routes')
+
+const timeLog = (req, res, next) => {
+  const date = new Date();
+  console.log(date)
+  console.log(req.method); 
+  next(); 
+};
+
+app.use(timeLog)
 
 app.use('/', routes)
 
@@ -12,3 +19,15 @@ app.listen(PORT, () => {
   console.log('Все есть')
 });
 
+const express = require('express');
+const routes = require('./routes.js');
+
+
+  console.log(date)
+    console.log(req.method);
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер:');
+  console.log(BASE_PATH);
+});
