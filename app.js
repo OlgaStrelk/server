665ecd4c199b21f2591bf89249518eb326c5ebe4
Strelkova Olga <strelod@yandex.ru>
2022-06-22 15:11:11 +0300

    add mongoose

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express');
+const express = require("express");
 const { PORT = 3000 } = process.env;
 const app = express();
-const routes = require('./routes')
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+
+// подключаюсь к севреру монго
+mongoose.connect('mongodb://localhost:27017/mydb')
 
 const timeLog = (req, res, next) => {
   const date = new Date();
-  console.log(date)
-  console.log(req.method); 
-  next(); 
+  console.log(date);
+  console.log(req.method);
+  next();
 };
 
-app.use(timeLog)
+app.use(timeLog);
 
-app.use('/', routes)
+app.use("/", routes);
 
-routes.get()
+routes.get();
 app.listen(PORT, () => {
-  console.log('Все есть')
+  console.log("Все есть");
 });
 
-const express = require('express');
-const routes = require('./routes.js');
+const express = require("express");
+const routes = require("./routes.js");
 
-
-  console.log(date)
-    console.log(req.method);
-app.use('/', routes);
+console.log(date);
+console.log(req.method);
+app.use("/", routes);
 
 app.listen(PORT, () => {
-  console.log('Ссылка на сервер:');
+  console.log("Ссылка на сервер:");
   console.log(BASE_PATH);
 });
